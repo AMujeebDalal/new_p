9fc1d619556bc3293f06f22a6302d0102f340137
Abdul Mujeeb Dalal <14127415+AMujeebDalal@users.noreply.github.com>
2020-11-27 11:02:12 +0530

    Update reeborg_maze.py

--- reeborg_maze.py
+++ reeborg_maze.py
@@ -2,6 +2,11 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+
 while at_goal() != True:
     if right_is_clear():
         turn_right()
@@ -10,26 +15,4 @@ while at_goal() != True:
         move()
     else:
         turn_left()  
-            
-            
-            
 
-            
-            
-            
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-while at_goal() != True:
-    while right_is_clear():
-        turn_right()
-        if front_is_clear():
-            move()
-    while wall_on_right():
-        if front_is_clear():
-            move()
-        else:
-            turn_left()
-            if front_is_clear():
-                move()
